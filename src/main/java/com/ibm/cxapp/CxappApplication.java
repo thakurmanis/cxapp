a5da899923a748769f521cf173c77ec82bdb41a6
Van Nguyen <vann586@gmail.com>
2017-12-04 19:01:29 -0500

    First Commit

---
+++ src/main/java/com/ibm/cxapp/CxappApplication.java
@@ -0,0 +1,12 @@
+package com.ibm.cxapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CxappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CxappApplication.class, args);
+	}
+}
